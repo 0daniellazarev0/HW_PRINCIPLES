827c9839ac04adb6a86e779537e63d24058aeab4
Daniel Lazarev <daniil.lt@hotmail.com>
2024-09-05 20:15:43 +0300

    Task done

---
+++ src/main/java/ru/netology/Post.java
@@ -0,0 +1,14 @@
+package ru.netology;
+
+public class Post {
+    FormDate birthday = new FormDate();
+    // заполните другие поля даты рождения
+    public String name;
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public Boolean subscription;
+    // поле для имени
+    // добавьте другие поля
+}
